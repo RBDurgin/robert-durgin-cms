ddc974e7b91668f4d91cc2b46b95245a93f0e243
robert.durgin@synacor.com <robert.durgin@synacor.com>
2019-07-22 11:28:12 -0400

    Ported over social icons to use font-awesome.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faStackOverflow, faGithub } from '@fortawesome/free-brands-svg-icons'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -63,17 +64,8 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
@@ -84,7 +76,7 @@ const Navbar = class extends React.Component {
                 rel="noopener noreferrer"
               >
                 <span className="icon">
-                  <img src={github} alt="Github" />
+                  <FontAwesomeIcon icon={faGithub} color="black" />
                 </span>
               </a>
             </div>
